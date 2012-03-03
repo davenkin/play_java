2bb0e914b1f75e02f90feafc44e43656267b0719
Davenkin <davenkin@163.com>
2012-03-03 11:33:51 +0800

    add fishing game

---
+++ fishing-game/src/main/java/com/thoughtworks/davenkin/poker/Board.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.davenkin.poker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: 3/3/12
+ * Time: 11:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Board {
+}
---
+++ fishing-game/src/main/java/com/thoughtworks/davenkin/poker/PokerGame.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.davenkin.poker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: twer
+ * Date: 3/3/12
+ * Time: 11:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PokerGame {
+
+}
