a3f5f381907613d89b76481b1973ee482226b63b
Davenkin <davenkin@163.com>
2012-05-26 14:11:55 +0800

    use jndi instead of Naming to fetch romote object

--- rmi/src/com/thoughtworks/davenkin/rmi/client/HelloWorldClient.java
+++ rmi/src/com/thoughtworks/davenkin/rmi/client/HelloWorldClient.java
@@ -2,27 +2,23 @@ package com.thoughtworks.davenkin.rmi.client;
 
 import com.thoughtworks.davenkin.rmi.server.HelloWorld;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.rmi.RemoteException;
+import java.util.Hashtable;
 
 public class HelloWorldClient
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws NamingException, RemoteException
     {
-        try{
-           HelloWorld helloWorld = (HelloWorld) Naming.lookup("rmi://localhost:1099/HelloWorldService");
-            System.out.println(helloWorld.sayHelloWorld());
-        } catch (RemoteException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (MalformedURLException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (NotBoundException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+
+        Hashtable env = new Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+        InitialContext context = new InitialContext(env);
+        HelloWorld helloWorld = (HelloWorld) context.lookup("/HelloWorldService");
+
+        System.out.println(helloWorld.sayHelloWorld());
+
     }
 }
--- rmi/src/com/thoughtworks/davenkin/rmi/server/HelloWorldServer.java
+++ rmi/src/com/thoughtworks/davenkin/rmi/server/HelloWorldServer.java
@@ -1,20 +1,33 @@
 package com.thoughtworks.davenkin.rmi.server;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.util.Hashtable;
 
 public class HelloWorldServer
 {
-    public HelloWorldServer() {
-        try {
-            HelloWorld c = new HelloWorldImpl();
-            Naming.rebind("rmi://localhost:1099/HelloWorldService", c);
-        } catch (Exception e) {
-            System.out.println("Trouble: " + e);
-        }
+    private final InitialContext context;
+
+    public HelloWorldServer() throws NamingException
+    {
+        Hashtable env = new Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+        context = new InitialContext(env);
+
     }
 
-    public static void main(String args[]) {
-        new HelloWorldServer();
+    public void createBinding() throws RemoteException, NamingException
+    {
+        context.bind("/HelloWorldService", new HelloWorldImpl());
+    }
+
+    public static void main(String args[]) throws NamingException, RemoteException
+    {
+        HelloWorldServer helloWorldServer = new HelloWorldServer();
+        helloWorldServer.createBinding();
     }
 }
